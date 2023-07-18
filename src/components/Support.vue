f37333ad90d5ab2e04365e251a98bc503e4b0a26
ahakbsl <ahak.bsl@gmail.com>
2023-07-18 19:37:40 +0600

    responsive issue fixed in support center

--- src/components/Support.vue
+++ src/components/Support.vue
@@ -1,95 +1,97 @@
 <template>
-  <div class="px-6 py-24 bg-white sm:py-32 lg:px-8">
-    <div class="max-w-2xl mx-auto text-center">
-      <p class="text-base font-semibold leading-7 text-indigo-600">
-        Get the help you need
-      </p>
-      <h2 class="mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-        Support center
-      </h2>
-      <p class="mt-6 text-lg leading-8 text-gray-600">
-        Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem
-        cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.
-      </p>
+  <div>
+    <div class="px-6 py-24 bg-white sm:py-32 lg:px-8">
+      <div class="max-w-2xl mx-auto text-center">
+        <p class="text-base font-semibold leading-7 text-indigo-600">
+          Get the help you need
+        </p>
+        <h2 class="mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+          Support center
+        </h2>
+        <p class="mt-6 text-lg leading-8 text-gray-600">
+          Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem
+          cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.
+        </p>
+      </div>
     </div>
-  </div>
-  <div class="grid grid-cols-1 gap-x-8 gap-y-10 lg:grid-cols-3">
-    <div>
-      <h2 class="text-3xl font-bold tracking-tight text-gray-900">
-        Get in touch
-      </h2>
-      <p class="mt-4 leading-7 text-gray-600">
-        Quam nunc nunc eu sed. Sed rhoncus quis ultricies ac pellentesque.
-      </p>
-    </div>
-    <div class="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:col-span-2 lg:gap-8">
-      <div class="p-10 rounded-2xl bg-gray-50">
-        <h3 class="text-base font-semibold leading-7 text-gray-900">
-          Collaborate
-        </h3>
-        <dl class="mt-3 space-y-1 text-sm leading-6 text-gray-600">
-          <div>
-            <dt class="sr-only">Email</dt>
-            <dd>
-              <a class="font-semibold text-indigo-600" href="mailto:collaborate@example.com">collaborate@example.com</a>
-            </dd>
-          </div>
-          <div class="mt-1">
-            <dt class="sr-only">Phone number</dt>
-            <dd>+1 (555) 905-2345</dd>
-          </div>
-        </dl>
+    <div class="grid grid-cols-1 px-6 gap-x-8 gap-y-10 lg:grid-cols-3">
+      <div>
+        <h2 class="text-3xl font-bold tracking-tight text-gray-900">
+          Get in touch
+        </h2>
+        <p class="mt-4 leading-7 text-gray-600">
+          Quam nunc nunc eu sed. Sed rhoncus quis ultricies ac pellentesque.
+        </p>
       </div>
-      <div class="p-10 rounded-2xl bg-gray-50">
-        <h3 class="text-base font-semibold leading-7 text-gray-900">
-          Press
-        </h3>
-        <dl class="mt-3 space-y-1 text-sm leading-6 text-gray-600">
-          <div>
-            <dt class="sr-only">Email</dt>
-            <dd>
-              <a class="font-semibold text-indigo-600" href="mailto:press@example.com">press@example.com</a>
-            </dd>
-          </div>
-          <div class="mt-1">
-            <dt class="sr-only">Phone number</dt>
-            <dd>+1 (555) 905-3456</dd>
-          </div>
-        </dl>
-      </div>
-      <div class="p-10 rounded-2xl bg-gray-50">
-        <h3 class="text-base font-semibold leading-7 text-gray-900">
-          Join our team
-        </h3>
-        <dl class="mt-3 space-y-1 text-sm leading-6 text-gray-600">
-          <div>
-            <dt class="sr-only">Email</dt>
-            <dd>
-              <a class="font-semibold text-indigo-600" href="mailto:careers@example.com">careers@example.com</a>
-            </dd>
-          </div>
-          <div class="mt-1">
-            <dt class="sr-only">Phone number</dt>
-            <dd>+1 (555) 905-4567</dd>
-          </div>
-        </dl>
-      </div>
-      <div class="p-10 rounded-2xl bg-gray-50">
-        <h3 class="text-base font-semibold leading-7 text-gray-900">
-          Say hello
-        </h3>
-        <dl class="mt-3 space-y-1 text-sm leading-6 text-gray-600">
-          <div>
-            <dt class="sr-only">Email</dt>
-            <dd>
-              <a class="font-semibold text-indigo-600" href="mailto:hello@example.com">hello@example.com</a>
-            </dd>
-          </div>
-          <div class="mt-1">
-            <dt class="sr-only">Phone number</dt>
-            <dd>+1 (555) 905-5678</dd>
-          </div>
-        </dl>
+      <div class="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:col-span-2 lg:gap-8">
+        <div class="p-10 rounded-2xl bg-gray-50">
+          <h3 class="text-base font-semibold leading-7 text-gray-900">
+            Collaborate
+          </h3>
+          <dl class="mt-3 space-y-1 text-sm leading-6 text-gray-600">
+            <div>
+              <dt class="sr-only">Email</dt>
+              <dd>
+                <a class="font-semibold text-indigo-600" href="mailto:collaborate@example.com">collaborate@example.com</a>
+              </dd>
+            </div>
+            <div class="mt-1">
+              <dt class="sr-only">Phone number</dt>
+              <dd>+1 (555) 905-2345</dd>
+            </div>
+          </dl>
+        </div>
+        <div class="p-10 rounded-2xl bg-gray-50">
+          <h3 class="text-base font-semibold leading-7 text-gray-900">
+            Press
+          </h3>
+          <dl class="mt-3 space-y-1 text-sm leading-6 text-gray-600">
+            <div>
+              <dt class="sr-only">Email</dt>
+              <dd>
+                <a class="font-semibold text-indigo-600" href="mailto:press@example.com">press@example.com</a>
+              </dd>
+            </div>
+            <div class="mt-1">
+              <dt class="sr-only">Phone number</dt>
+              <dd>+1 (555) 905-3456</dd>
+            </div>
+          </dl>
+        </div>
+        <div class="p-10 rounded-2xl bg-gray-50">
+          <h3 class="text-base font-semibold leading-7 text-gray-900">
+            Join our team
+          </h3>
+          <dl class="mt-3 space-y-1 text-sm leading-6 text-gray-600">
+            <div>
+              <dt class="sr-only">Email</dt>
+              <dd>
+                <a class="font-semibold text-indigo-600" href="mailto:careers@example.com">careers@example.com</a>
+              </dd>
+            </div>
+            <div class="mt-1">
+              <dt class="sr-only">Phone number</dt>
+              <dd>+1 (555) 905-4567</dd>
+            </div>
+          </dl>
+        </div>
+        <div class="p-10 rounded-2xl bg-gray-50">
+          <h3 class="text-base font-semibold leading-7 text-gray-900">
+            Say hello
+          </h3>
+          <dl class="mt-3 space-y-1 text-sm leading-6 text-gray-600">
+            <div>
+              <dt class="sr-only">Email</dt>
+              <dd>
+                <a class="font-semibold text-indigo-600" href="mailto:hello@example.com">hello@example.com</a>
+              </dd>
+            </div>
+            <div class="mt-1">
+              <dt class="sr-only">Phone number</dt>
+              <dd>+1 (555) 905-5678</dd>
+            </div>
+          </dl>
+        </div>
       </div>
     </div>
   </div>
